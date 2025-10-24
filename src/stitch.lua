1613399b5adf8910fcf707609ab3330f1849b8f0
hertogp <pieter@dwark.nl>
2025-10-24 23:26:18 +0200

    refactoring

--- src/stitch.lua
+++ src/stitch.lua
@@ -1,6 +1,6 @@
 --[[ stitch ]]
 
-local M = {} -- returned by global Stitch() for testing
+local M = {} -- returned by global stitch() for testing
 local ctx = {} --> set per doc, holds meta.stitch (i.e. per doc)
 local opts = { log = "info" } --> set per cb being processed
 local level = {
@@ -16,8 +16,8 @@ local dump = require("dump") -- tmp, delme
 local F = string.format
 local pd = require("pandoc")
 
-local function log(log, fmt, ...)
-	if level[opts.log] >= level[log] then
+local function log(lvl, fmt, ...)
+	if level[opts.log] >= level[lvl] then
 		local text = F("[stitch %5s] (%s) " .. fmt .. "\n", log, opts.cid or "mod", ...)
 		io.stderr:write(text)
 	end
@@ -65,7 +65,7 @@ local hardcoded = {
 	-- bash: $@ is list of args, ${@: -1} is last argument
 }
 
--- Extract specific data from AST elements into lua table(s)
+-- extract specific data from ast elements into lua table(s)
 ---@param elm any either `doc.blocks`, `doc.meta` or a `CodeBlock`
 ---@return any regular table holding the metadata as lua values
 local function metalua(elm)
@@ -89,7 +89,7 @@ local function metalua(elm)
 		return elm
 	elseif "nil" == ptype then
 		return nil
-	elseif "Attr" == ptype then
+	elseif "attr" == ptype then
 		local t = {
 			identifier = elm.identifier,
 			classes = metalua(elm.classes),
@@ -100,7 +100,7 @@ local function metalua(elm)
 		end
 		return t
 	elseif "CodeBlock" == elm.tag then
-		-- a CodeBlock's type is actually 'Block' (and it's not the only one)
+		-- a codeblock's type is actually 'block' (and it's not the only one)
 		return {
 			text = elm.text,
 			attr = metalua(elm.attr),
@@ -136,7 +136,7 @@ local function mksha(cb)
 end
 
 -- create conditions for codeblock execution and set opts.exe
----@param cb table pandoc CodeBlock
+---@param cb table pandoc codeblock
 ---@return boolean ok success indicator
 local function mkexe(cb)
 	-- create dirs for cb's possible output files
@@ -162,21 +162,21 @@ local function mkexe(cb)
 	end
 	fh:close()
 
-	-- REVIEW: not platform independent
+	-- review: not platform independent
 	if not os.execute("chmod u+x " .. opts.cbx) then
 		log("error", "could not mark executable: " .. opts.cbx)
 		return false
 	end
 
-	-- REVIEW: check expanse complete, no more #<names> left?
+	-- review: check expanse complete, no more #<names> left?
 	opts.exe = opts.cmd:gsub("%#(%w+)", opts)
 	return true
 end
 
--- read file `name` and, possibly, convert to pandoc AST using `format`
+-- read file `name` and, possibly, convert to pandoc ast using `format`
 ---@param name string file to read
----@param format? string convert file data to AST using a pandoc reader ("" to skip)
----@return string|table? data file data, AST or nil (in case of errors)
+---@param format? string convert file data to ast using a pandoc reader ("" to skip)
+---@return string|table? data file data, ast or nil (in case of errors)
 local function fread(name, format)
 	local ok, dta
 	local fh, err = io.open(name, "r")
@@ -203,50 +203,7 @@ local function fread(name, format)
 	return dta
 end
 
--- run doc through lua filter(s), count how many were actually applied
----@param doc any file data, pandoc AST or nil
----@param filter string name of lua mod.fun to run (if any)
----@return string|table? doc the, possibly, modified doc
----@return number count the number of filters actually applied
-local function fluaf(doc, filter)
-	local count = 0
-	local ok, filters, tmp
-	if #filter == 0 then
-		return doc, count
-	end
-
-	local mod, fun = filter:match("(%w+)%.?(%w*)")
-	fun = #fun > 0 and fun or "Pandoc" -- default to mod.Pandoc
-	ok, filters = pcall(require, mod)
-	if not ok then
-		log("warn", F("skipping @%s: module %s not found", filter, mod))
-		return doc, count
-	elseif filters == true then
-		log("warn", F("skipping @%s: not a list of filters", filter))
-		return doc, count
-	end
-
-	if doc and "Pandoc" == pd.utils.type(doc) then
-		doc.meta.stitch = pd.MetaMap(ctx)
-	end
-
-	for n, f in ipairs(filters) do
-		if f[fun] then
-			ok, tmp = pcall(f[fun], doc)
-			if not ok then
-				log("warn", F("filter '%s[%s].%s', failed, filter ignored", mod, n, fun))
-			else
-				doc = tmp
-				count = count + 1
-			end
-		else
-			log("warn", F("filter '%s[%d].%s', '%s' not found, filter ignored", mod, n, fun))
-		end
-	end
-	return doc, count
-end
-
--- save `doc` to given `fname`, except when it's an AST
+-- save `doc` to given `fname`, except when it's an ast
 ---@param doc string|table? doc to be saved
 ---@param fname string filename to save doc with
 ---@return boolean ok success indicator
@@ -276,8 +233,8 @@ local function fsave(doc, fname)
 end
 
 -- clones `cb`, removes its stitch properties, adds a 'stitched' class
----@param cb table a CodeBlock instance
----@return table clone a new CodeBlock instance
+---@param cb table a codeblock instance
+---@return table clone a new codeblock instance
 local function mkfcb(cb)
 	local clone = cb:clone()
 
@@ -295,11 +252,54 @@ local function mkfcb(cb)
 	return clone
 end
 
---[[ AST ]]
+-- run doc through lua filter(s), count how many were actually applied
+---@param doc any file data, pandoc ast or nil
+---@param filter string name of lua mod.fun to run (if any)
+---@return string|table? doc the, possibly, modified doc
+---@return number count the number of filters actually applied
+local function xform(doc, filter)
+	local count = 0
+	local ok, filters, tmp
+	if #filter == 0 then
+		return doc, count
+	end
+
+	local mod, fun = filter:match("(%w+)%.?(%w*)")
+	fun = #fun > 0 and fun or "pandoc" -- default to mod.pandoc
+	ok, filters = pcall(require, mod)
+	if not ok then
+		log("warn", F("skipping @%s: module %s not found", filter, mod))
+		return doc, count
+	elseif filters == true then
+		log("warn", F("skipping @%s: not a list of filters", filter))
+		return doc, count
+	end
+
+	if doc and "Pandoc" == pd.utils.type(doc) then
+		doc.meta.stitch = pd.metamap(ctx)
+	end
+
+	for n, f in ipairs(filters) do
+		if f[fun] then
+			ok, tmp = pcall(f[fun], doc)
+			if not ok then
+				log("warn", F("filter '%s[%s].%s', failed, filter ignored", mod, n, fun))
+			else
+				doc = tmp
+				count = count + 1
+			end
+		else
+			log("warn", F("filter '%s[%d].%s', '%s' not found, filter ignored", mod, n, fun))
+		end
+	end
+	return doc, count
+end
+
+--[[ ast ]]
 
 -- create doc elements for codeblock
 ---@param cb table codeblock
----@return table result sequence of pandoc AST elements
+---@return table result sequence of pandoc ast elements
 local function result(cb)
 	local elms, count = {}, 0
 
@@ -308,7 +308,7 @@ local function result(cb)
 		local fname = opts[what]
 		if fname then
 			local doc = fread(opts[what], format)
-			doc, count = fluaf(doc, filter)
+			doc, count = xform(doc, filter)
 			if count > 0 or true then
 				-- a filter could post-process an image so save it, if applicable
 				fsave(doc, opts[what])
@@ -321,7 +321,7 @@ local function result(cb)
 			local elmid = F("%s-%d-%s", opts.cid, idx, what)
 			ncb.identifier = elmid
 			if "fcb" == how and "Pandoc" == pd.utils.type(doc) then
-				log("debug", "%s, '%s:%s', data as native AST", elmid, what, how)
+				log("debug", "%s, '%s:%s', data as native ast", elmid, what, how)
 				ncb.text = pd.write(doc, "native")
 				elms[#elms + 1] = ncb
 			elseif "fcb" == how and "cbx" == what then
@@ -334,17 +334,17 @@ local function result(cb)
 				ncb.text = doc
 				elms[#elms + 1] = ncb
 			elseif "img" == how then
-				log("debug", "%s, inc '%s:%s', Image %s", elmid, what, how, fname)
+				log("debug", "%s, inc '%s:%s', image %s", elmid, what, how, fname)
 				elms[#elms + 1] = pd.Image({ caption }, fname, title, ncb.attr)
 			elseif "fig" == how then
-				log("debug", "%s, inc '%s:%s', Figure", elmid, what, how, fname)
+				log("debug", "%s, inc '%s:%s', figure", elmid, what, how, fname)
 				local img = pd.Image({ caption }, fname, title, ncb.attr)
 				elms[#elms + 1] = pd.Figure(img, { caption }, ncb.attr)
 			elseif doc and "" == how then
 				-- output elements cbx, out, err, art without a how
 				if "Pandoc" == pd.utils.type(doc) then
-					-- an AST by default has its individual blocks inserted
-					log("debug", "%s, inc '%s:%s', AST blocks", elmid, what, how)
+					-- an ast by default has its individual blocks inserted
+					log("debug", "%s, inc '%s:%s', ast blocks", elmid, what, how)
 					if doc.blocks[1].attr then
 						doc.blocks[1].identifier = ncb.identifier -- or blocks[1].attr = ncb.attr
 						doc.blocks[1].classes = ncb.classes
@@ -353,12 +353,12 @@ local function result(cb)
 						elms[#elms + 1] = block
 					end
 				else
-					-- doc is raw data and inserted as a Div
-					log("debug", "%s, inc '%s:%s', data as Div", elmid, what, how)
+					-- doc is raw data and inserted as a div
+					log("debug", "%s, inc '%s:%s', data as div", elmid, what, how)
 					elms[#elms + 1] = pd.Div(doc, ncb.attr)
 				end
 			else
-				-- TODO: never reached?
+				-- todo: never reached?
 				log("error", "%s, inc '%s:%s' skipped, data is '%s'", elmid, what, how, doc)
 				elms[#elms + 1] = pd.Div(F("?? %s, %s:%s: unknown or no output seen", elmid, what, how), ncb.attr)
 			end
@@ -402,9 +402,9 @@ local function mkopt(cb)
 end
 
 --- extract `doc.meta.stitch` config from a doc's meta block (if any)
----@param doc table the doc's AST
+---@param doc table the doc's ast
 ---@return table config doc.meta.stitch's named configs: option,value-pairs
-function M.context(doc)
+local function mkctx(doc)
 	-- pickup named cfg sections in meta.stitch, resolution order:
 	-- opts (cb) -> ctx (stitch[cb.cfg]) -> defaults -> hardcoded
 	ctx = metalua(doc.meta.stitch or {}) or {}
@@ -429,8 +429,8 @@ function M.context(doc)
 	return ctx
 end
 
----@poram cb a pandoc.CodeBlock
----@return any list of nodes in pandoc's AST
+---@poram cb a pandoc.codeblock
+---@return any list of nodes in pandoc's ast
 ---@return string? err non-empty string in case of errors, or nil
 function M.codeblock(cb)
 	if not cb.classes:find("stitch") then
@@ -451,16 +451,16 @@ end
 --[[ main ]]
 
 log("info", F("PANDOC_VERSION %s", _ENV.PANDOC_VERSION)) -- 3.1.3
--- assert(PANDOC_API_VERSION >= {1, 23}, "need at least pandoc x.x.x")
+-- assert(pandoc_api_version >= {1, 23}, "need at least pandoc x.x.x")
 
 local function pandoc(doc)
-	ctx = M.context(doc)
+	ctx = mkctx(doc)
 	return doc:walk({ CodeBlock = M.codeblock })
 end
 
--- TODO: for testing
--- * create local S = {} w/ all stitch func/data in it
--- * return { {Pandoc = pandoc}, 0 = S } }
+-- todo: for testing
+-- * create local s = {} w/ all stitch func/data in it
+-- * return { {pandoc = pandoc}, 0 = s } }
 
 return {
 	-- a filter is a list of filters
